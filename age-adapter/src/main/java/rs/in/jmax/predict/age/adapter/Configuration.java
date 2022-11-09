08dab06c8a2f1dca9143e168dc378845c99ed2e7
Vlade Maksimovic <maxogm@gmail.com>
2022-11-09 15:40:47 +0100

    Add configuration properties

---
+++ age-adapter/src/main/java/rs/in/jmax/predict/age/adapter/Configuration.java
@@ -0,0 +1,8 @@
+package rs.in.jmax.predict.age.adapter;
+
+import org.springframework.context.annotation.PropertySource;
+
+@org.springframework.context.annotation.Configuration("rs.in.jmax.predict.age.adapter.Configuration")
+@PropertySource("classpath:age-adapter.properties")
+public class Configuration {
+}
